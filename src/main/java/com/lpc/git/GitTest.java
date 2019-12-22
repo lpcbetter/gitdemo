a9c0700e565c139fa494307723deb2435596491e
lipengcheng <lpcbetter@163.com>
2019-12-22 14:40:37 +0800

    增加了一个吃的方法

--- src/main/java/com/lpc/git/GitTest.java
+++ src/main/java/com/lpc/git/GitTest.java
@@ -9,4 +9,10 @@ public class GitTest {
     
          System.out.println("git 工具类修改了");
     }
+     public void eat(){
+        int a=0;
+        int b=0;
+
+         System.out.println("猫吃鱼");
+     }
 }
